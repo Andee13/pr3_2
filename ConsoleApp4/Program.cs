7a63ed17ed84d29d187ab08bef00b3d2c28e259e
Andee13 <39676479+Andee13@users.noreply.github.com>
2019-03-24 20:45:24 +0200

    Initial commit

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ComsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "hello ";
+            string world = "world";
+            string helloWorld = hello + world;
+            Console.WriteLine(helloWorld);
+
+            world = "cat";
+            String rat = world.Insert(0, "r");
+            Console.WriteLine(rat);
+            string newWorld = helloWorld.Replace(hello, "hi ");
+            Console.WriteLine(newWorld);
+
+            String afterremove = helloWorld.Remove(5);
+            Console.WriteLine("afterRemove = " + afterremove);
+            Console.WriteLine("equals = " + helloWorld.Equals(hello + world));
+
+            String a = "abc";
+            String b = "nbs abc";
+            Console.WriteLine("The second symbol i s equal?"+ (a[1] == b[1]));
+            
+            Console.WriteLine(b.IndexOf(a));
+        }
+
+           
+            
+        
+    }
+}
